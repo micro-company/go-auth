7c03261d72a742a9c66ecbbdb3e2771eba70e9cd
Login Victor <batazor111@gmail.com>
2017-11-16 03:12:58 +0300

    Fix message for validation - uniqUser

--- handlers/user/validation.go
+++ handlers/user/validation.go
@@ -19,7 +19,7 @@ func CheckUniqueUser(w http.ResponseWriter, user userModel.User) bool {
 
 	if count > 0 {
 		w.WriteHeader(http.StatusBadRequest)
-		utils.Error(w, errors.New("need unique mail"))
+		utils.Error(w, errors.New(`{"mail":"need unique mail"}`))
 		return true
 	}
 
